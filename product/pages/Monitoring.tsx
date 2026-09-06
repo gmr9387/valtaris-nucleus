8e86a092b9e88c9dab65b7c2aaf844dc4ea6be03
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:56:56 -0400

    Update Monitoring.tsx

--- product/pages/Monitoring.tsx
+++ product/pages/Monitoring.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MonitoringPage } from "../../ui/pages/MonitoringPage";
+import { fetchMetrics } from "../data/monitoring";
 
 export const Monitoring = ({ navItems, active, onSelect }) => {
+  const [metrics, setMetrics] = useState([]);
+
+  useEffect(() => {
+    fetchMetrics().then(setMetrics);
+  }, []);
+
   return (
     <MonitoringPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
-      metrics={[]}
+      metrics={metrics}
     />
   );
 };
