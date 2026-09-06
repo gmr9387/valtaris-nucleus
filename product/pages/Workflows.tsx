c8a3aaa5b0bd488911102eea4ef99047a12713be
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:56:11 -0400

    Update Workflows.tsx

--- product/pages/Workflows.tsx
+++ product/pages/Workflows.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WorkflowPage } from "../../ui/pages/WorkflowPage";
+import { fetchWorkflows } from "../data/workflows";
 
 export const Workflows = ({ navItems, active, onSelect }) => {
+  const [workflows, setWorkflows] = useState([]);
+
+  useEffect(() => {
+    fetchWorkflows().then(setWorkflows);
+  }, []);
+
   return (
     <WorkflowPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
-      workflows={[]}
+      workflows={workflows}
     />
   );
 };
