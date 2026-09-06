297523be655a066cd24f6fe5878686540035db25
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:55:46 -0400

    Update Dashboard.tsx

--- product/pages/Dashboard.tsx
+++ product/pages/Dashboard.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DashboardPage } from "../../ui/pages/DashboardPage";
+import { fetchMetrics } from "../data/monitoring";
 
 export const Dashboard = ({ navItems, active, onSelect }) => {
+  const [metrics, setMetrics] = useState([]);
+
+  useEffect(() => {
+    fetchMetrics().then(setMetrics);
+  }, []);
+
   return (
     <DashboardPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
-      metrics={[]}
+      metrics={metrics}
     />
   );
 };
