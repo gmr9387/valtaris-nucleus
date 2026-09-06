92b87c92c2c60535cbe7d834f01abf29882e8f96
George M. Rios <gmr9387@gmail.com>
2026-09-06 19:26:00 -0400

    Create updateRole.ts

---
+++ product/flows/updateRole.ts
@@ -0,0 +1,9 @@
+export const updateRole = async (payload: { email: string; role: string }) => {
+  const res = await fetch("/api/identity/role/update", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  });
+
+  return res.json();
+};
