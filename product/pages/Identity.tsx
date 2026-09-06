ec0e0382b2debca3f98cef8a68863acdf58b802e
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:58:06 -0400

    Update Identity.tsx

--- product/pages/Identity.tsx
+++ product/pages/Identity.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IdentityPage } from "../../ui/pages/IdentityPage";
+import { fetchIdentities } from "../data/identity";
 
 export const Identity = ({ navItems, active, onSelect }) => {
+  const [identities, setIdentities] = useState([]);
+
+  useEffect(() => {
+    fetchIdentities().then(setIdentities);
+  }, []);
+
   return (
     <IdentityPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
-      identities={[]}
+      identities={identities}
     />
   );
 };
