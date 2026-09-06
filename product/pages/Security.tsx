56c52deb393ace4c3eecf481ec39e27f3ffd19c0
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:57:42 -0400

    Update Security.tsx

--- product/pages/Security.tsx
+++ product/pages/Security.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SecurityPage } from "../../ui/pages/SecurityPage";
+import { fetchSecurityEvents } from "../data/security";
 
 export const Security = ({ navItems, active, onSelect }) => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchSecurityEvents().then(setEvents);
+  }, []);
+
   return (
     <SecurityPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
-      events={[]}
+      events={events}
     />
   );
 };
