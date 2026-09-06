48f502853efe227ec2b37aeb067069780568eaf1
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:56:34 -0400

    Update Payments.tsx

--- product/pages/Payments.tsx
+++ product/pages/Payments.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PaymentsPage } from "../../ui/pages/PaymentsPage";
+import { fetchPayments } from "../data/payments";
 
 export const Payments = ({ navItems, active, onSelect }) => {
+  const [payments, setPayments] = useState([]);
+
+  useEffect(() => {
+    fetchPayments().then(setPayments);
+  }, []);
+
   return (
     <PaymentsPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
-      payments={[]}
+      payments={payments}
     />
   );
 };
