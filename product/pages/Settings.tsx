6a27b170c49e546fbe0d142a520454040e7a377b
George M. Rios <gmr9387@gmail.com>
2026-09-06 18:58:53 -0400

    Update Settings.tsx

--- product/pages/Settings.tsx
+++ product/pages/Settings.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SettingsPage } from "../../ui/pages/SettingsPage";
+import { fetchSettings } from "../data/settings";
 
 export const Settings = ({ navItems, active, onSelect }) => {
+  const [settings, setSettings] = useState({ name: "" });
+
+  useEffect(() => {
+    fetchSettings().then(setSettings);
+  }, []);
+
   return (
     <SettingsPage
       navItems={navItems}
       active={active}
       onSelect={onSelect}
       form={{
-        values: {},
+        values: settings,
         errors: {},
         set: () => {},
         onSubmit: () => {},
