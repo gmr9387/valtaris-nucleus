2d925c661e93b2709a60d27c984c624ae6d2f5f0
George M. Rios <gmr9387@gmail.com>
2026-09-06 01:25:47 -0400

    Create typography.ts

---
+++ ui/tokens/typography.ts
@@ -0,0 +1,19 @@
+export const typography = {
+  fontFamily: {
+    primary: "AeroTech, sans-serif",
+    secondary: "Inter, sans-serif",
+  },
+  sizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 20,
+    xl: 24,
+    xxl: 32,
+  },
+  weight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+};
